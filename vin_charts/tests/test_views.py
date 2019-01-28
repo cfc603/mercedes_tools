e99734272266751c4444709bc0cc14b6656eca0d
Trevor <wtrevor162@gmail.com>
2019-01-27 22:03:26 -0500

    add VehicleDetail view

--- vin_charts/tests/test_views.py
+++ vin_charts/tests/test_views.py
@@ -2,11 +2,22 @@ from django.test import TestCase
 
 from mock import patch
 
-from vin_charts.models import ModelYear
-from vin_charts.views import VehicleList
+from vin_charts.models import ModelYear, Vehicle
+from vin_charts.views import VehicleDetail, VehicleList
 
 
 class VehicleListViewTest(TestCase):
+
+    @patch("vin_charts.views.get_object_or_404")
+    def test_get_object(self, get_object_or_404):
+        # setup
+        view = VehicleDetail()
+        view.kwargs = {"pk": 1}
+        obj = view.get_object()
+
+        # asserts
+        get_object_or_404.assert_called_once_with(Vehicle, pk=1)
+        self.assertEqual(obj, get_object_or_404(Vehicle, pk=1))
 
     @patch("vin_charts.views.ListView.get_queryset")
     @patch("vin_charts.views.get_object_or_404", return_value=1234)
--- vin_charts/views.py
+++ vin_charts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from vin_charts.models import ModelYear, Vehicle
 
@@ -9,11 +9,21 @@ class ModelYearList(ListView):
     model = ModelYear
 
 
+class VehicleDetail(DetailView):
+
+    model = Vehicle
+
+    def get_object(self):
+        return get_object_or_404(Vehicle, pk=self.kwargs["pk"])
+
+
 class VehicleList(ListView):
 
     model = Vehicle
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        self.model_year = get_object_or_404(ModelYear, year=self.kwargs["model_year"])
+        self.model_year = get_object_or_404(
+            ModelYear, year=self.kwargs["model_year"]
+        )
         return queryset.filter(model_year=self.model_year)
