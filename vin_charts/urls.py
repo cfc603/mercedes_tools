0f0d062ce3f7b8da9df57bdac51c405b357d3f85
Trevor <wtrevor162@gmail.com>
2019-01-27 21:15:31 -0500

    add ModelYearList view

--- vin_charts/urls.py
+++ vin_charts/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from vin_charts import views
 
 urlpatterns = [
+    path("model-years/", views.ModelYearList.as_view(), name="model_year_list"),
 ]
--- vin_charts/views.py
+++ vin_charts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from vin_charts.views import ModelYear
+
+
+class ModelYearList(ListView):
+
+    model = ModelYear
