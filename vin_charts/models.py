1dcd3be760958479a1722e7289671126b54aee34
Trevor <wtrevor162@gmail.com>
2019-01-27 21:07:35 -0500

    build out models

--- vin_charts/models.py
+++ vin_charts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Chassis(models.Model):
+
+    number = models.CharField(max_length=7, unique=True)
+
+    def __str__(self):
+        return self.number
+
+
+class Engine(models.Model):
+
+    number = models.CharField(max_length=7, unique=True)
+
+    def __str__(self):
+        return self.number
+
+
+class ModelYear(models.Model):
+
+    year = models.PositiveSmallIntegerField(unique=True)
+
+    def __str__(self):
+        return str(self.year)
+
+
+class Transmission(models.Model):
+
+    number = models.CharField(max_length=7, unique=True)
+
+    def __str__(self):
+        return self.number
+
+
+class Vehicle(models.Model):
+
+    model_year = models.ForeignKey("ModelYear", on_delete=models.PROTECT)
+    vin_prefix = models.CharField(max_length=10, unique=True)
+    sales_designation = models.CharField(max_length=120)
+    chassis = models.ForeignKey("Chassis", on_delete=models.PROTECT)
+    engine = models.ForeignKey("Engine", on_delete=models.PROTECT)
+    transmission = models.ForeignKey("Transmission", on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.model_year.year} {self.sales_designation}"
--- vin_charts/tests/test_models.py
+++ vin_charts/tests/test_models.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
 
-# Create your tests here.
+from model_mommy import mommy
+
+class ChassisModelTest(TestCase):
+
+    def test_str(self):
+        # setup
+        obj = mommy.make("vin_charts.Chassis")
+
+        # tests
+        self.assertEqual(obj.__str__(), obj.number)
+
+
+class EngineModelTest(TestCase):
+
+    def test_str(self):
+        # setup
+        obj = mommy.make("vin_charts.Engine")
+
+        # tests
+        self.assertEqual(obj.__str__(), obj.number)
+
+
+class ModelYearModelTest(TestCase):
+
+    def test_str(self):
+        # setup
+        obj = mommy.make("vin_charts.ModelYear")
+
+        # tests
+        self.assertEqual(obj.__str__(), str(obj.year))
+
+
+class TransmissionModelTest(TestCase):
+
+    def test_str(self):
+        # setup
+        obj = mommy.make("vin_charts.Transmission")
+
+        # tests
+        self.assertEqual(obj.__str__(), obj.number)
+
+
+class VehicleModelTest(TestCase):
+
+    def test_str(self):
+        # setup
+        obj = mommy.make("vin_charts.Vehicle")
+
+        # tests
+        self.assertEqual(
+            obj.__str__(), f"{obj.model_year.year} {obj.sales_designation}"
+        )
