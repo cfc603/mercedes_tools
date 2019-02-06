63490846058fb32436e270ecf3cb7f0c872ab353
Trevor <wtrevor162@gmail.com>
2019-02-06 13:14:39 -0500

    add parts models

--- parts/models.py
+++ parts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from vin_charts.models import Chassis, Engine, Transmission
+
+
+class Part(models.Model):
+
+    sku = models.CharField(max_length=50, unique=True)
+    type = models.ForeignKey("Type", on_delete=models.PROTECT)
+    chassis = models.ManyToManyField(Chassis)
+    engines = models.ManyToManyField(Engine)
+    transmissions = models.ManyToManyField(Transmission)
+
+    def __str__(self):
+        return self.sku
+
+
+class Type(models.Model):
+
+    description = models.CharField(max_length=120, unique=True)
+
+    def __str__(self):
+        return self.description
--- parts/tests/test_models.py
+++ parts/tests/test_models.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from model_mommy import mommy
+
+
+class PartModelTest(TestCase):
+
+    def test_str(self):
+        # setup
+        obj = mommy.make("parts.Part")
+
+        # setup
+        self.assertEqual(obj.__str(), obj.sku)
+
+
+class PartModelTest(TestCase):
+
+    def test_str(self):
+        # setup
+        obj = mommy.make("parts.Type")
+
+        # setup
+        self.assertEqual(obj.__str(), obj.description)
